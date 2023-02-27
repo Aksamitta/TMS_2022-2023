18be6ef1e202dc85ad7043b51dd7979cf971156e
Aksana.Patrubeika <oksana-potrubeiko@yandex.ru>
2023-02-27 22:02:28 +0300

    .

--- Lab15_Aksana.Patrubeika_Controllers/Lab15_Aksana.Patrubeika_Controllers/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab15_Aksana.Patrubeika_Controllers.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Lab17_Aksana.Patrubeika_Views/Lab17_Aksana.Patrubeika_Views/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab17_Aksana.Patrubeika_Views.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
